210f6bfa95829ba9df5852b5cb0a040945ad2149
Luke Morris <lukevmorris@gmail.com>
2018-09-07 20:09:24 -0700

    add certfile

--- lib/bypass/instance.ex
+++ lib/bypass/instance.ex
@@ -267,7 +267,7 @@ defmodule Bypass.Instance do
   defp do_up(port, cert, ref) do
     plug_opts = [self()]
     {:ok, {:sslsocket, nil, {socket, _}}} = :ranch_ssl.listen(ip: @listen_ip, port: port, cert: cert)
-    cowboy_opts = [ref: ref, acceptors: 5, port: port, socket: socket, keyfile: "./testkey.pem"]
+    cowboy_opts = [ref: ref, acceptors: 5, port: port, socket: socket, keyfile: "./testkey.pem", certfile: "./testcert.pem"]
     {:ok, _pid} = Plug.Adapters.Cowboy.https(Bypass.Plug, plug_opts, cowboy_opts)
     socket
   end
